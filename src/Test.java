3349f53a246ae30b31673fce5830dace0f01420e
WangChen-PC\WangChen <chen_glassyuki@163.com>
2018-10-23 20:06:15 +0800

    字符串连接用于哈夫曼编码

--- src/Test.java
+++ src/Test.java
@@ -22,6 +22,8 @@ public class Test {
 			int number = Node.minNode(chas);
 			Node temp = new Node();
 			temp.leftChild = chas.get(number);
+			String tempSerialNumber = "";
+			//temp.leftChild.value.setNumberSerial(ns);
 			chas.remove(chas.get(number));
 			number = Node.minNode(chas);
 			temp.rightChild = chas.get(number);
@@ -31,5 +33,12 @@ public class Test {
 		}
 		Node.preOrder(chas.get(0));
 		//System.out.println(chas.get(0).rightChild.leftChild.value.getRate());
+		System.out.println(addString("aaaa", "aass"));
 	}
+	//×Ö·û´®Á¬½Ó
+    public static String addString(String text, String addstring) {
+        StringBuilder sb = new StringBuilder(text);
+        sb.append(addstring);
+        return sb.toString();
+    }
 }
