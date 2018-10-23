93efbf58b9316e20e14fbfa20d704401a6ec6b58
WangChen-PC\WangChen <chen_glassyuki@163.com>
2018-10-23 20:00:06 +0800

    加上先序遍历

--- src/Node.java
+++ src/Node.java
@@ -9,5 +9,41 @@ import java.util.ArrayList;
  *
  */
 public class Node {
-
+	public Node leftChild;
+	public Node rightChild;
+	public Character value;
+	Node(){
+		value = new Character();
+	}
+	Node(Character v){
+		value = v;
+	}
+	public static int minNode(ArrayList<Node> al){
+		Node min = al.get(0);
+		int number = 0;
+		for (int i = 0; i< al.size(); i++){
+			if(min.value.getRate() > al.get(i).value.getRate()){
+				min = al.get(i);
+				number = i;
+				//System.out.println(al.get(i).value.getRate());
+			}
+		}
+		return number;
+	}
+	
+	public void setValue(){
+		if(leftChild != null && rightChild != null){
+			int number = (leftChild.value.getRate() + rightChild.value.getRate());
+			//System.out.println(number);
+			this.value.setRate(number);
+		}
+	}
+	
+	public static void preOrder(Node n){
+		if(n != null){
+			 System.out.print(n.value.getRate()+"  ");  
+	         preOrder(n.leftChild);  
+	         preOrder(n.rightChild); 
+		}
+	}
 }
--- src/Test.java
+++ src/Test.java
@@ -10,5 +10,26 @@ import java.util.List;
  *
  */
 public class Test {
-
+	public static void main(String[] args) {
+		Character c1 = new Character('A',"",10);
+		Character c2 = new Character('B',"",15);
+		Character c3 = new Character('C',"",9);
+		ArrayList<Node> chas = new ArrayList<Node>();
+		chas.add(new Node(c1));
+		chas.add(new Node(c2));
+		chas.add(new Node(c3));
+		for (int i = 0; i < chas.size(); i++){
+			int number = Node.minNode(chas);
+			Node temp = new Node();
+			temp.leftChild = chas.get(number);
+			chas.remove(chas.get(number));
+			number = Node.minNode(chas);
+			temp.rightChild = chas.get(number);
+			chas.remove(chas.get(number));
+			temp.setValue();			
+			chas.add(temp);
+		}
+		Node.preOrder(chas.get(0));
+		//System.out.println(chas.get(0).rightChild.leftChild.value.getRate());
+	}
 }
